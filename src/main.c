99beab14fd45c134c86acb600b5d0f6eaf1d7af7
zAndreu <amarcab1@upv.edu.es>
2026-08-26 00:23:40 +0200

    Implementado test para regresion lineal simple con operaciones matriciales

--- src/main.c
+++ src/main.c
@@ -1,79 +1,9 @@
 #include <stdio.h>
 #include "matrix.h"
 
-#include <stdio.h>
-#include <stddef.h>
-
 int main(void)
 {
-    double x[] = {
-        0.0,
-        1.0,
-        2.0,
-        3.0,
-        4.0,
-        5.0,
-        6.0,
-        7.0,
-        8.0,
-        9.0
-    };
-
-    double y[] = {
-        1.0,
-        3.0,
-        5.0,
-        7.0,
-        9.0,
-        11.0,
-        13.0,
-        15.0,
-        17.0,
-        19.0
-    };
-
-    size_t samples = sizeof(x) / sizeof(x[0]);
-
-    double weight = 0.0;
-    double bias = 0.0;
-
-    double learning_rate = 0.01;
-    size_t epochs = 1000;
-
-    double total_loss = 0;
-    double gradient_weight = 0;
-    double gradient_bias = 0;
-    double prediction = 0;
-
-    for (size_t epoch = 0; epoch < epochs; epoch++) {
-        total_loss = 0;
-        gradient_weight = 0;
-        gradient_bias = 0;
-        prediction = 0;
-        for (size_t idx = 0; idx < samples; idx++) {
-            prediction = weight * x[idx] + bias;
-            double error = prediction - y[idx];
-            total_loss += error * error;
-            gradient_weight += 2.0 * error * x[idx];
-            gradient_bias += 2.0 * error;
-        }
-        total_loss /= samples;
-        gradient_weight /= samples;
-        gradient_bias /= samples;
-
-        weight -= learning_rate * gradient_weight;
-        bias -= learning_rate * gradient_bias;
-    }
-
-    printf("Peso final: %f\n", weight);
-    printf("Bias final: %f\n", bias);
-
-    double new_x = 10.0;
-    double new_prediction = weight * new_x + bias;
-    double new_error = new_prediction - 21.0;
-    double new_total_loss = new_error * new_error;
-    printf("Predicción para x = %f: %f\n", new_x, new_prediction);
-    printf("Error: %f\n", new_total_loss);
-
+    // Proximamente
     return 0;
 }
+// End of file
---
+++ test/test_linear_matrix.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main(void)
+{
+    double x_values[] = {
+        0.0,
+        1.0,
+        2.0,
+        3.0,
+        4.0,
+        5.0,
+        6.0,
+        7.0,
+        8.0,
+        9.0
+    };
+
+    double y_values[] = {
+        1.0,
+        3.0,
+        5.0,
+        7.0,
+        9.0,
+        11.0,
+        13.0,
+        15.0,
+        17.0,
+        19.0
+    };
+
+    size_t samples = sizeof(x_values) / sizeof(x_values[0]);
+
+    matrix *x = matrix_create(samples, 1);
+    matrix *y = matrix_create(samples, 1);
+
+    for(size_t idx = 0; idx < (size_t)samples; idx++) {
+        matrix_set(x, idx, 0, x_values[idx]);
+        matrix_set(y, idx, 0, y_values[idx]);
+    }
+
+    matrix *x_transposed = matrix_create(1, samples);
+    matrix_transpose(x, x_transposed);
+
+    matrix *weight = matrix_create(1, 1);
+    matrix_fill(weight, 0.0);
+    matrix *bias = matrix_create(1, 1);
+    matrix_fill(bias, 0.0);
+
+    matrix *gradient_weight = matrix_create(1, 1);
+    matrix *gradient_bias = matrix_create(1, 1);
+
+    double learning_rate = 0.01;
+    size_t epochs = 1000;
+
+    double total_loss = 0;
+
+    matrix *prediction = matrix_create(samples, 1);
+    matrix *error = matrix_create(samples, 1);
+    matrix *squared_errors = matrix_create(samples, 1);
+
+    matrix *scaled_gradient_weight = matrix_create(1, 1);
+    matrix *scaled_gradient_bias = matrix_create(1, 1);
+
+    double gradient_factor = 2.0 / (double)samples;
+
+    for (size_t epoch = 0; epoch < epochs; epoch++) {
+
+        matrix_mult(x, weight, prediction);
+        matrix_add_row_vector(prediction, bias);
+
+        matrix_sub(prediction, y, error);
+        matrix_hadamard(error, error, squared_errors);
+        total_loss += matrix_mean(squared_errors);
+
+        matrix_mult(x_transposed, error, gradient_weight);
+        matrix_mult_scalar(gradient_weight, gradient_factor, gradient_weight);
+        
+        matrix *sum_errors = matrix_sum_rows(error);
+        matrix_mult_scalar(sum_errors, gradient_factor, gradient_bias);
+        matrix_free(sum_errors);
+
+        matrix_mult_scalar(gradient_weight, learning_rate, scaled_gradient_weight);
+        matrix_sub(weight, scaled_gradient_weight, weight);
+
+        matrix_mult_scalar(gradient_bias, learning_rate, scaled_gradient_bias);
+        matrix_sub(bias, scaled_gradient_bias, bias);
+    }
+
+    printf("Peso final:");
+    matrix_print(weight);
+    printf("Bias final:");
+    matrix_print(bias);
+
+    matrix_free(x);
+    matrix_free(y);
+    matrix_free(x_transposed);
+    matrix_free(weight);
+    matrix_free(bias);
+    matrix_free(gradient_weight);
+    matrix_free(gradient_bias);
+    matrix_free(prediction);
+    matrix_free(error);
+    matrix_free(squared_errors);
+    matrix_free(scaled_gradient_weight);
+    matrix_free(scaled_gradient_bias);
+
+    return 0;
+}
+// End of file
